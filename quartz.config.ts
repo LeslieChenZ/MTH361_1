b19e388f768724655fb31bde678b3b2c368703c1
Leslie Chen <zheng_chen_work@outlook.com>
2026-05-24 19:49:14 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "LeslieChenZ.github.io/MTH361_1",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
